c11dfe24b02044ff437a7e3155ff3574c2599351
Boban005 <bogibossss@gmail.com>
2024-06-22 18:56:27 +0200

    rotiraaaaa

--- Rabbits Jumping Rabbits/Assets/Scripts/DirectionScript.cs
+++ Rabbits Jumping Rabbits/Assets/Scripts/DirectionScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.UIElements;
 
 public class DirectionScript : MonoBehaviour
@@ -10,6 +12,7 @@ public class DirectionScript : MonoBehaviour
     public float Angle = 0;
     public float AngleSpeed;
     private int Smer = 1;
+    public float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +28,12 @@ public class DirectionScript : MonoBehaviour
     private void FixedUpdate()
     {
         Angle += Smer * Time.fixedDeltaTime * AngleSpeed;
-        if (Angle > 3.141 - 0.2) Smer = -1;
-        if (Angle < 0.2 ) Smer = 1;
+        if (Angle > 160) Smer = -1;
+        if (Angle < 20) Smer = 1;
 
-        Transform.position = new Vector3(Mathf.Cos(Angle) * 3, Mathf.Sin(Angle)*3, 0) + PlayerTransform.position;
+        Transform.position = new Vector3(Mathf.Cos(Angle / 180 * 3.141f) * distance, Mathf.Sin(Angle / 180 * 3.141f) *distance, 0) + PlayerTransform.position;
 
-
+        Transform.rotation = Quaternion.AngleAxis(Angle + 30, new Vector3(0, 0, 1));
 
     }
 
